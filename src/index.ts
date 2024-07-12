fb43d00392e502716c0d61c90818e60350a347c6
PeppeSavelli <peppe070904@gmail.com>
2024-07-12 17:32:54 +0200

    Fix: Update server

--- src/index.ts
+++ src/index.ts
@@ -68,5 +68,5 @@ routerApi.use("/users", routerUsers);
 app.use("/api", routerApi);
 
 app.listen(3000, function(){
-    console.log("Server is running on e-commerce-production-94c6.up.railway.app")
+    console.log("Server is running on e-commerce-production-95ec.up.railway.app")
 });
